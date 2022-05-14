db236de1fc46a68d9eef57451a2a8986f226db45
gicanon <100998384+gicanon@users.noreply.github.com>
2022-05-14 16:49:07 +0200

    Add files via upload

---
+++ Test 4 - item_count_from_index.py
@@ -0,0 +1,15 @@
+def item_count_from_index():
+    """Accpeting two inputs, a list and an integer-index
+    Return a count (number) of how many times the item at that index appears in the list
+    Return a empty string ("") if integer-index is out of bounds"""
+
+    input_elements = (input("Enter elements of a list separated by space: "))
+    user_list = input_elements.split()
+    try:
+        input_index = int(input("Enter a number as list index to count how many times the items at that index appears: "))
+        return user_list.count(user_list[input_index])
+    except ValueError:
+        return '""'
+
+print(item_count_from_index())
+    
